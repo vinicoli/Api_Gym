7119037ec91f5912d6985a8cd343c86000fef61a
Vinicius Oliveira <vinioliveira.web@gmail.com>
2023-07-27 01:30:07 -0300

    add wait_for_db command
    Create the wait_for_db command to wait for the database is ready to
    connect.

--- gym_api/gym_api/settings.py
+++ gym_api/gym_api/settings.py
@@ -12,11 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-# from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
@@ -88,16 +86,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT'),
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
