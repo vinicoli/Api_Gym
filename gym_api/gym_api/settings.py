09d6708c95b8626cad79886eadbb46937a09984a
Vinicius Oliveira <vinioliveira.web@gmail.com>
2023-07-26 23:09:57 -0300

    configure docker
    Configure docker to create image and configure the main service with
    docker-compose.

--- gym_api/gym_api/settings.py
+++ gym_api/gym_api/settings.py
@@ -12,11 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv()
+# load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'api',
-    'rest_framework'
 ]
 
 MIDDLEWARE = [
