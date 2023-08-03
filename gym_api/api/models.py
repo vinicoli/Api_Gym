1dbaefcb35d01825b4337bf81baabaf98bd1cc19
jonas peixoto <jonas.peixoto.087@gmail.com>
2023-08-03 10:30:56 -0300

    #3 Feat: Adicionando modelo de perfil dos usuarios

--- gym_api/api/models.py
+++ gym_api/api/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    email = models.CharField(max_length=100, blank=True)
+    data_de_nascimento = models.DateField(null=True, blank=False)
+    sexo = models.CharField(max_length=1, choices=[('M', 'Masculino'), ('F', 'Feminino'), ('O', 'Outro')], null=True, blank=True)
+    endereco_rua = models.CharField(max_length=100, blank=True)
+    endereco_cidade = models.CharField(max_length=100, blank=True)
+    endereco_estado = models.CharField(max_length=50, blank=True)
+    telefone = models.CharField(max_length=50, blank=True)
+
+
