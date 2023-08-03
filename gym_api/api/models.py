9397b6b5ba8f8949147884e13f3ac28f64c393d6
Vinicius Oliveira <vinioliveira.web@gmail.com>
2023-08-02 23:29:27 -0300

    add superuser support

--- gym_api/api/models.py
+++ gym_api/api/models.py
@@ -18,7 +18,16 @@ class UserManager(BaseUserManager):
             raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(using=self.db)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email=email, password=password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
         return user
 
